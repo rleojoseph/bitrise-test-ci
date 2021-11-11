f8099cc670cd2617c703c8bb14e06df4c53388fd
Leo Joseph <leojoseph.r@rakuten.com>
2021-11-11 17:41:37 +0900

    Sample project

---
+++ CITesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CITesting
+//
+//  Created by Leo, Joseph | MTSD on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
