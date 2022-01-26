8a1286d6a6ae494d9579975eee0adaac9c69dc7f
Leo Joseph <leojoseph.r@rakuten.com>
2022-01-26 15:02:04 +0900

    More testcases

--- CITesting/ViewController.swift
+++ CITesting/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     /// Method to print coverage info
     func showCoverageInfo() {
         print("")
+    }
+
+    func greet(person: String) -> String {
+        let greeting = "Hello " + person
+        return greeting
+    }
+
+    func getHelloWorld() -> String {
+        return "Hello World"
+    }
+
+    func stringToInt(string: String) -> Int {
+        return Int(string) ?? 0
     }
 }
--- CITestingTests/CITestingTests.swift
+++ CITestingTests/CITestingTests.swift
@@ -29,5 +29,19 @@ class CITestingTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    func testStringToInt() {
+        let vv = ViewController()
+        let num = vv.stringToInt(string: "12")
+        XCTAssertEqual(12, num)
+    }
+    func testStringToIntWithInvalidData() {
+        let vv = ViewController()
+        let num = vv.stringToInt(string: "Leo")
+        XCTAssertEqual(0, num)
+    }
+    func testHelloWorld() {
+        let vv = ViewController()
+        let num = vv.getHelloWorld()
+        XCTAssertEqual("Hello World", num)
+    }
 }
