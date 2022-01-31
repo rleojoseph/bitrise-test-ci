4a46b615fb2ac5cce808dda3318920bf7975d5f4
Leo Joseph <leojoseph.r@rakuten.com>
2022-02-01 02:45:00 +0900

    .

--- CITestingTests/CITestingTests.swift
+++ CITestingTests/CITestingTests.swift
@@ -5,16 +5,16 @@ import XCTest
 
 class CITestingTests: XCTestCase {
 
-    func testStringToInt() {
-        let vv = ViewController()
-        let num = vv.stringToInt(string: "12")
-        XCTAssertEqual(12, num)
-    }
-    func testStringToIntWithInvalidData() {
-        let vv = ViewController()
-        let num = vv.stringToInt(string: "Leo")
-        XCTAssertEqual(0, num)
-    }
+    // func testStringToInt() {
+    //     let vv = ViewController()
+    //     let num = vv.stringToInt(string: "12")
+    //     XCTAssertEqual(12, num)
+    // }
+    // func testStringToIntWithInvalidData() {
+    //     let vv = ViewController()
+    //     let num = vv.stringToInt(string: "Leo")
+    //     XCTAssertEqual(0, num)
+    // }
     func testHelloWorld() {
         let vv = ViewController()
         let num = vv.getHelloWorld()
