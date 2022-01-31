66f645ec8ace27ebc72c835dbdfd00d40e562494
Leo Joseph <leojoseph.r@rakuten.com>
2022-02-01 03:17:32 +0900

    .

--- fastlane/actions/publish_code_coverage.rb
+++ fastlane/actions/publish_code_coverage.rb
@@ -66,21 +66,6 @@ module Fastlane
           UI.message("Code coverage updated for commit #{commit_sha}")
         end
       end
-      #####################################################
-      # @!group Documentation
-      #####################################################
-
-      def self.description
-        "Publish code coverage to a Github commit status check"
-      end
-
-      def self.details
-        "This action uses Slather to generate a code coverage report for the current Xcode project status, gets the coverage number, gets the current commit and publish the coverage number via Github API to a status check, making it available to see in pull requests, for example"
-      end
-
-      def self.authors
-        ["Federico Jordan"]
-      end
 
       def self.is_supported?(platform)
         platform == :ios
