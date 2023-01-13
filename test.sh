2b81c9414dfc27bd7891a452cc82b4bace80d6fa
Leo Joseph <leojoseph.r@rakuten.com>
2023-01-13 12:59:41 +0900

    Test

---
+++ test.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Usage info
+show_help()
+{
+echo "
+        Usage: [-v Type]
+
+        -t Type           Type
+        -p Build Prefix   prefix
+        -d                Displays useful data to debug this script
+        -a                Automatic mode. Requires -v parameter to be 100% without prompt
+        -s                Same as -a but in silent mode
+
+        -h                Help
+"
+}
+NO_PROMPT=0
+
+while getopts ":t:build-prefix:app-center-group:dhas" opt; do
+  case $opt in
+    t) TYPE="$OPTARG"
+    ;;
+    prefix) PREFIX="$OPTARG"
+    ;;
+    group-name) GROUP_NAME="$OPTARG"
+    d) set -ex
+    ;;
+    h) show_help; exit 0
+    ;;
+    a) NO_PROMPT=1
+    ;;
+    s) NO_PROMPT=1; exec &>/dev/null
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&1; exit 1
+    ;;
+  esac
+done
+
+
+WORK_DIR=$(pwd)
+TMP_DIR=$WORK_DIR/tmp
+
+echo "Build Prefix: $PREFIX"
+echo "Group Name: $GROUP_NAME"
